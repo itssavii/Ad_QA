4708688dfecbb43ff6533d90d12a826f688b6db1
itssavii <89682751+itssavii@users.noreply.github.com>
2021-10-16 08:50:51 +0530

    POM FW
    POM FW

---
+++ java/utilities/ConstantValue.java
@@ -0,0 +1,9 @@
+package utilities;
+
+public class ConstantValue {
+	public static final String userName="saviy21@rediffmail.com";
+	public static final String password="Raj@1234";
+	//public static final String userName="anshulc55";
+	//public static final String password="Test@1234";
+
+}
